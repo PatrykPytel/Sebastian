f1db63e5bb6f7e9f508627a800369508a66a73f5
Patryk Pytel <patrykpytel26@gmail.com>
2024-10-11 11:18:19 +0200

    sa

--- v5/hello.py
+++ v5/hello.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from markupsafe import escape
 app = Flask(__name__)
 @app.route('/')
 def say_hello():
@@ -16,3 +17,24 @@ def hello():
     <p>NKONKONISONII</p> 
     </body>        """
     return tekst
+@app.route('/path/<path:y>')
+def pathy(y):
+    x = y.lower()
+    return f"<h2 style='color: red'>sciezka = {x}</h2>"
+@app.route('/calc/<int:zm1>')
+def calc(zm1):
+    return f"{zm1} * 10 = {zm1 * 10}"
+@app.route('/Result')
+def about(): 
+    return render_template('about.html')
+@app.route('/tree')
+def drzewo():
+    return render_template('drzewo.html')
+@app.route('/user/<username>')
+def user(username):
+    y = "Patryk"
+    x = username.upper()
+    if f"{username}" == y : 
+        return f"<h1>Witaj Administratorze</h1>"
+    else: 
+        return f"<h2>CZesc {escape(x)}</h2>"
